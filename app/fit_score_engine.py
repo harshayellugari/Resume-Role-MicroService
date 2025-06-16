31a2fab44a4e0e7955be762a4877202ef4b8d03b
harshayellugari <harshayellugari@gmail.com>
2025-06-16 09:04:44 +0530

    Refactor compute_fit_score function to accept skills and req_skills as parameters

--- app/fit_score_engine.py
+++ app/fit_score_engine.py
@@ -1,7 +1,9 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-def compute_fit_score(resume_text, job_description):
+def compute_fit_score(skills, req_skills):
+    resume_text = " ".join(skills)
+    job_description = " ".join(req_skills)
     corpus = [resume_text, job_description]
     vectorizer = TfidfVectorizer()
     vectors = vectorizer.fit_transform(corpus)
